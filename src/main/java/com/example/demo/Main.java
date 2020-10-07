b307ef884d929bba179609c1f2effffc8823cda4
mackhood <germanjugo@hotmail.com>
2020-10-07 02:33:26 -0300

    modifico estructura del proyecto

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 @SpringBootApplication
-public class DemoApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
